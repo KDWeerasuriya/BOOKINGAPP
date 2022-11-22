ce18f0c95e142703bb452a122e8fca02c646421a
KD Weerasuriya <dilshankelum535@gmail.com>
2022-11-22 20:03:13 +0530

    add con

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { register,login } from "../contrillers/auth.js";
+import { register,login } from "../controllers/auth.js.js";
 
 const router = express.Router();
 
